39e93407d1eb14ea74bba2db79acc387b16ea9b2
h181207 <h181207@stud.hib.no>
2020-09-06 22:03:11 +0200

    Refactored: Extracted local variables and methods

--- dat159-refactoring/src/dat159/fowler/Customer.java
+++ dat159-refactoring/src/dat159/fowler/Customer.java
@@ -16,35 +16,22 @@ public class Customer {
         int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
+        // extracting method here Movie. It includes the code needed to as desccribed in the name.
+        //Adding rentals as parameter becasuse they are local in the scope
+
         while (rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-
+            //Extracting the local variable this movie
+            Movie thisMovie = each.getMovie();
+            
             // determine amount for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
-            // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                    each.getDaysRented() > 1) frequentRenterPoints ++;
-
+            //extracting method to calculate thisAmount
+            thisAmount = calculateTotalAmount(each);
+            
+            frequentRenterPoints += calculateFRP(each);
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" +
+            result += "\t" + thisMovie.getTitle()+ "\t" +
                     String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
@@ -53,6 +40,39 @@ public class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) +
                 " frequent renter points";
         return result;
+    }
+    
+    private double calculateTotalAmount(Rental each) {
+        Movie thisMovie = each.getMovie();
+		double thisAmount = 0;
+
+        switch (thisMovie.getPriceCode()) {
+	        case Movie.REGULAR:
+	            thisAmount += 2;
+	            if (each.getDaysRented() > 2)
+	                thisAmount += (each.getDaysRented() - 2) * 1.5;
+	            break;
+	        case Movie.NEW_RELEASE:
+	            thisAmount += each.getDaysRented() * 3;
+	            break;
+	        case Movie.CHILDRENS:
+	            thisAmount += 1.5;
+	            if (each.getDaysRented() > 3)
+	                thisAmount += (each.getDaysRented() - 3) * 1.5;
+	            break;
+        }
+        return thisAmount;
+    }
+    
+    private double calculateFRP(Rental each) {
+    	double frequentRenterPoints = 1;
+        Movie thisMovie = each.getMovie();
+        // add bonus for a two day new release rental
+
+        if ((thisMovie.getPriceCode() == Movie.NEW_RELEASE) &&
+                each.getDaysRented() > 1) frequentRenterPoints ++;
+
+        return frequentRenterPoints;	
     }
 
     public void addRental(Rental arg) {
